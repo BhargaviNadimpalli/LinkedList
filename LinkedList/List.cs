ec7ca242d2eb5db231a607a3eab1abac2f1555a3
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-14 20:44:15 -0600

    feat : Inserted values in linkedlist

---
+++ LinkedList/List.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class List
+    { 
+            internal Node head;
+            internal void Add(int data)
+            {
+                //Object of Node
+                Node node = new Node(data);
+
+                //Add Data
+                if (this.head == null)
+                    this.head = node;
+                else
+                {
+                    Node temp = head;
+                    while (temp.next != null)
+                    {
+                        temp = temp.next;
+                    }
+                    temp.next = node;
+                }
+                 Console.WriteLine("{0} Inserted in the Linked List", node.data);
+            }
+        }
+    }
+
+
--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -7,6 +7,10 @@ namespace LinkedList
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to linked list programs");
+            List list = new List();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
         }
     }
 }
